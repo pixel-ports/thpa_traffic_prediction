ea5ba0c50e51e6ce585b91e819619a713f3bfd34
Ignacio Lacalle <iglaub@upv.es>
2019-09-06 10:10:15 +0200

    Add 2 documents (.py)

---
+++ objective1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import pandas as pd
+import requests as requests
+import json
+import ijson
+import sys
+
+print 'Number of arguments: '+len(sys.argv)+' arguments.'
+print 'Argument List: '+str(sys.argv)
+
+#Instantiating and giving value to the interval that wants to be converted into the baseline format
+date = sys.argv[1]
+time_start = sys.argv[2]
+time_end = sys.argv[3]
+interval = sys.argv[4]                    
+
+#Making the call to the server where the Historical Data is stored --- This should be changed in the future to be read from the PIXEL Information Hub and not directly from
+#the raw data source provided by THPA
+#The call is made to the URL with the date_start (format accepted by the web service)
+raw_data = requests.get("https://download.thpa.gr/pixel/traffic.php?dd="+ date)
+    #ijson will iteratively parse the json file instead of reading it all in at once.
+    #This is slower than directly reading the whole file in, but it enables us to work with
+    #large files that can’t fit in memory
+with open(raw_data. 'r') as f:
+    objects = ijson.items(f,'meta.view.columns.item')
+    vehicles = list(objects)
+    print(vehicles[0])
+
+#Filtering the data according to the values provided in sys.argv[1 & 2]
+#https://www.dataquest.io/blog/python-json-tutorial/
+data_json = raw_data
+
+my_new_json
+#Calculation of the volume associated to the interval
+
+
+#Construction of the baseline format (according to the instructions by Dejan)
+
+#Storing the baseline dataset
